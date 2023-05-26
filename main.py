a6e408b1c93417f498df519fb2261f3569c8ba4f
Mariusz Kolodziej <makolo@microsoft.com>
2023-05-25 23:49:13 -0400

    new push

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Super Mario World"}
+    return {"message": "Hello Super Mario World 2"}
 
 if __name__ == '__main__':
     uvicorn.run('main:app')
