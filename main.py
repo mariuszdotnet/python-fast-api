958669074d567465d935f06202507cec21002200
Mariusz Kolodziej <makolo@microsoft.com>
2023-06-01 00:25:26 -0400

    Finished Request Body

--- main.py
+++ main.py
@@ -3,12 +3,19 @@
 
 from enum import Enum
 from fastapi import FastAPI
+from pydantic import BaseModel
 import uvicorn
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 app = FastAPI()
 
@@ -48,9 +55,9 @@ async def read_file(file_path: str):
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 2):
-    return fake_items_db[skip + limit]
+# @app.get("/items/")
+# async def read_item(skip: int = 0, limit: int = 2):
+#     return fake_items_db[skip + limit]
 
 # @app.get("/items/{item_id}")
 # async def read_item(item_id: str, q: str | None = None):
@@ -87,10 +94,43 @@ async def read_user_item(
 #     item = {"item_id": item_id, "needy": needy}
 #     return item
 
-@app.get("/items/{item_id}")
-async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: int | None = None):
-    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
-    return item
+# @app.get("/items/{item_id}")
+# async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: int | None = None):
+#     item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+#     return item
+
+# Rwquest Body
+
+# Sample JSON
+# {
+#     "name": "Foo",
+#     "description": "An optional description",
+#     "price": 45.2,
+#     "tax": 3.5
+# }
+
+# @app.post("/items/")
+# async def create_item(item: Item):
+#     return item
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+# @app.put("/items/{item_id}")
+# async def create_item(item_id: int, item: Item):
+#     return {"item_id": item_id, **item.dict()}
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
